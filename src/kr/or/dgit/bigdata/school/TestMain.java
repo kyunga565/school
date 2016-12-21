d053ed3a219d978f9dad16e4a402cda614bf5ed0
kyunga <dnldzmdnldzm@naver.com>
2016-12-21 14:43:46 +0900

    testmain connectionFactory create

---
+++ src/kr/or/dgit/bigdata/school/TestMain.java
@@ -0,0 +1,13 @@
+package kr.or.dgit.bigdata.school;
+
+import java.sql.Connection;
+
+import kr.or.dgit.bigdata.school.util.ConnectionFactory;
+
+public class TestMain {
+	public static void main(String[] args) {
+		Connection con = ConnectionFactory.getInstance();
+		
+		System.out.println(con);
+	}
+}
